1eec9f4800bc7062d0093963e9e352611b4326b9
Andriy F <forspam.smith@gmail.com>
2014-04-21 00:20:12 +0300

    Log is being scrolled to bottom

--- HashCheckerProj/HashChecker.Designer.cs
+++ HashCheckerProj/HashChecker.Designer.cs
@@ -235,6 +235,7 @@
             this.rtbLog.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
+            this.rtbLog.HideSelection = false;
             this.rtbLog.Location = new System.Drawing.Point(0, 134);
             this.rtbLog.Name = "rtbLog";
             this.rtbLog.ReadOnly = true;
