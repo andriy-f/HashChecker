dee9f9da4ba54207254f4ce707ee5a227a7a2336
Andriy Fetsyuk <forspam.smith@gmail.com>
2018-02-23 12:40:10 +0200

    Build refactor

--- build.bat
+++ build.bat
@@ -1,4 +1,2 @@
-CALL "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\Tools\VsDevCmd.bat"
-
-MSBuild .\HashChecker.sln /p:Configuration=Release /p:Platform=x86
-MSBuild .\HashChecker.sln /p:Configuration=Release /p:Platform=x64
+MSBuild %~dp0HashCheckerSetup\HashCheckerSetup.wixproj /p:Configuration=Release /p:Platform=x86
+MSBuild %~dp0HashCheckerSetup\HashCheckerSetup.wixproj /p:Configuration=Release /p:Platform=x64
